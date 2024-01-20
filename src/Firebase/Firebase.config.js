b8cb82930fdb57198d2b6d460e7470f3eb330518
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-21 03:13:05 +0600

    project structure done

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQDTmsdfF2nocWuEYee8jTkyRgWb7VIX8",
+  authDomain: "react-event-management-18b9d.firebaseapp.com",
+  projectId: "react-event-management-18b9d",
+  storageBucket: "react-event-management-18b9d.appspot.com",
+  messagingSenderId: "390410183184",
+  appId: "1:390410183184:web:8693430006c0c8d322e095",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>This is home</h1>
+    </div>
+  );
+};
+
+export default Home;
